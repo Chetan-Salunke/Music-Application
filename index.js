7665f16ea18ad012797cdaea842b46f9bd37486d
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-12-31 04:10:57 +0530

    final-commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// function toggleMusic1() {
+//     var audio = document.getElementById("arjanvailly");
+//     // Toggle play/pause
+//     if (audio.paused) {
+//         audio.play();
+//     } else {
+//         audio.pause();
+//     }
+// }
+//toggle menu//
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+//direct music js
+let currentAudio = null;
+
+function playSong(songName) {
+    if (currentAudio) {
+        currentAudio.pause();
+    }
+
+
+    // Create a new audio element
+    const audio = new Audio();
+
+    // Set the source of the audio element based on the selected song
+    if (songName === 'song1') {
+        audio.src = './songs/Arjan Vailly Animal 128 Kbps.mp3';
+    } else if (songName === 'song2') {
+        audio.src = './songs/Softly-(PagalWorld).mp3';
+    }
+    else if (songName === 'song3') {
+        audio.src = './songs/Jamal Kudu - Animal-(PagalWorld.Com.IN).mp3';
+    }
+    else if (songName === 'song4') {
+        audio.src = './songs/Obsessed(PagalWorld.com.pe).mp3';
+    }
+
+    // Play the audio
+    audio.play();
+
+    
+    
+    // Update the currentAudio variable to the newly created audio element
+    currentAudio = audio;
+}
